4f3e5a0c3e8d8c8a5b59731acbd483baf2c86087
hoangbayern <hoangdm2002@gmail.com>
2023-09-12 00:11:12 +0700

    feature about me

---
+++ components/Heading-h2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+    children: string,
+}
+
+export default function SectionHeading({children}: Props) {
+  return (
+    <h2 className='font-bold text-3xl capitalize mb-8'>{children}</h2>
+  )
+}
